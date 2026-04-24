f0cff69190bcdab122399bff85d765f915270746
André Luiz Santiago <100866468+andrelssouza@users.noreply.github.com>
2026-04-24 08:35:28 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const fetch = require('node-fetch');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
